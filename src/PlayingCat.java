14c6d93bb5ef5f48228e041dccd98fbe2ddbd3fc
kittimorvai <kittimorv@gmail.com>
2020-06-08 14:03:57 +0200

    Initial commit

---
+++ src/PlayingCat.java
@@ -0,0 +1,19 @@
+public class PlayingCat {
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(false, 28));
+    }
+
+    public static boolean isCatPlaying (boolean summer, int temperature){
+        if (summer){
+            if ( temperature >= 25 && temperature <= 45){
+                return true;
+            }
+        } else if (!summer) {
+            if ( temperature >= 25 && temperature <= 35 ){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
